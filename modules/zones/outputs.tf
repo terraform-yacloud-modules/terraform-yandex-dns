efb00b96a01ffc78f46ba7e121fa73cc6b33cf32
Eugene Selivanov <sea.groundwork@gmail.com>
2025-01-10 18:43:35 +0700

    fix: remove unsupported outputs

--- modules/zones/outputs.tf
+++ modules/zones/outputs.tf
@@ -1,16 +1,6 @@
 output "yandex_dns_zone_id" {
   description = "Zone ID of DNS zone"
-  value       = { for k, v in yandex_dns_zone.this : k => v.zone_id }
-}
-
-output "yandex_dns_zone_name_servers" {
-  description = "Name servers of DNS zone"
-  value       = { for k, v in yandex_dns_zone.this : k => v.name_servers }
-}
-
-output "yandex_dns_zone_primary_name_server" {
-  description = "The DNS name server that created the SOA record."
-  value       = { for k, v in yandex_dns_zone.this : k => v.primary_name_server }
+  value       = { for k, v in yandex_dns_zone.this : k => v.id }
 }
 
 output "yandex_dns_zone_name" {
