a7e4f83dca8860228b6d3b5dca9b08cc253c3e94
Eugene Selivanov <sea.groundwork@gmail.com>
2025-05-05 17:08:13 +0700

    refactor: remove unused JSON output for Yandex DNS recordset

--- modules/recordset/outputs.tf
+++ modules/recordset/outputs.tf
@@ -11,17 +11,3 @@ output "yandex_dns_recordset" {
     }
   }
 }
-
-output "yandex_dns_recordset_jsonencoded" {
-  description = "DNS record attributes encoded as JSON string"
-  value = jsonencode({
-    for k, v in yandex_dns_recordset.this : k => {
-      id      = v.id
-      name    = v.name
-      type    = v.type
-      ttl     = v.ttl
-      data    = v.data
-      zone_id = v.zone_id
-    }
-  })
-}
